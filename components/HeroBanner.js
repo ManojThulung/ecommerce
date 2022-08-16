e7be5e8f8583394846c4d889259470986a468435
Manoj Rai <manojthulung03@gmail.com>
2022-08-16 17:09:14 +0545

    initial commit

---
+++ components/HeroBanner.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Link from "next/link";
+
+function HeroBanner() {
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src="" alt="ukelele" className="hero-banner-image" />
+        <div>
+          <Link href="/product/Id">
+            <button type="button">BUTTON TEXT</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>DESCRIPTION</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroBanner;
